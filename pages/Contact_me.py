1e65100ceec581335497e787c973724cffa7c3a3
ZxPower145 <119930303+ZxPower145@users.noreply.github.com>
2023-05-20 01:51:49 +0200

    Created Contact_me.py

---
+++ pages/Contact_me.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from send_email import send_email
+
+st.header("Contact me if you like my work :")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address: ")
+    subject = st.text_input("Enter the subject: ")
+    raw_message = st.text_area("Enter your message: ")
+    message = f"""\
+Subject: {subject}
+
+From: {user_email}
+{raw_message}
+"""
+    sub_btn = st.form_submit_button("Send")
+    if sub_btn:
+        send_email(message)
+        st.info("Email sent successfully!")
