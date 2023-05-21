f9454d8551aeea7ef0d028bf74ee4c9e3895789b
ZxPower145 <119930303+ZxPower145@users.noreply.github.com>
2023-05-21 22:11:45 +0200

    Update send_email.py

--- files/send_email.py
+++ files/send_email.py
@@ -3,14 +3,13 @@ import ssl
 import os
 
 
-def send_email(message):
+def send_email(message, receiver="costinbogdan145@gmail.com"):
     host = "smtp.gmail.com"
     port = 465
 
     username = "costinbogdan145@gmail.com"
     password = os.getenv("PASS")
 
-    receiver = "costinbogdan145@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
